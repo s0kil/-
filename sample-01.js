314a9c172b6ff33d41c4d4ff185500b158ccd27b
Daniel Sokil <dtechtours@gmail.com>
2020-05-12 17:51:54 -0400

    Update Sample 01

--- sample-01.js
+++ sample-01.js
@@ -2,6 +2,8 @@ import λ from "./elam.js";
 
 const sum = λ.memoize((a, b) => a + b);
 
+const addOne = λ.memoize((number) => number + 1);
+
 // Initial
 const r1 = sum(9, 9);
 console.log(r1);
@@ -9,3 +11,7 @@ console.log(r1);
 // Memoized
 const r2 = sum(9, 9);
 console.log(r2);
+
+// One Parameter
+const r3 = addOne(2);
+console.log(r3);
